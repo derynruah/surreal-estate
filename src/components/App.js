29fb71c244f57553d86b301f8638e8fe31753ba8
Deryn Rushworth <derynrushworth@gmail.com>
2023-02-14 21:19:27 +0000

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> 
+          Surreal Estate
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
